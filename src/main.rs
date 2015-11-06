b4c6eb9567fb8c7c94a048d354478e9528e01421
Jake Worth <jakeworth82@gmail.com>
2015-11-06 14:52:29 -0600

    Accept user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Heads or tails?");
+    println!("Please input your choice (h or t):");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
