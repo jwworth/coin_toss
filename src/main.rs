cad116bcf9c0636cc7b797b0be1761b979e7ebd7
Jake Worth <jakeworth82@gmail.com>
2015-11-06 15:02:47 -0600

    Accept user input and compare to number

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     println!("Heads or tails?");
     println!("Please input your choice (0 or 1):");
-
-    let number = rand::thread_rng().gen_range(0,1);
-    println!("The coin toss result is: {}", number);
 
     let mut guess = String::new();
 
@@ -16,5 +14,18 @@ fn main() {
         .ok()
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .ok()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    let number = rand::thread_rng().gen_range(0,2);
+    println!("The coin toss result is: {}", number);
+
+    match guess.cmp(&number) {
+          Ordering::Less    => println!("You lose!"),
+          Ordering::Greater => println!("You lose!"),
+          Ordering::Equal   => println!("You win!"),
+    }
 }
